4316e8e500dbb3f8df86efc63eff3b62e0f240e3
Ankit Sachan <ankit@rewardz.sg>
2021-03-31 08:57:13 +0530

    framework project added

---
+++ RewardzCommonComponents/RewardzCommonComponents/RewardzCommonComponents.h
@@ -0,0 +1,18 @@
+//
+//  RewardzCommonComponents.h
+//  RewardzCommonComponents
+//
+//  Created by Ankit Sachan on 31/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RewardzCommonComponents.
+FOUNDATION_EXPORT double RewardzCommonComponentsVersionNumber;
+
+//! Project version string for RewardzCommonComponents.
+FOUNDATION_EXPORT const unsigned char RewardzCommonComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RewardzCommonComponents/PublicHeader.h>
+
+

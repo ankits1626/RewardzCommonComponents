57652df0fc1f5b459357d8973e3c1abbb142eefd
ankits1626 <ankit_seeking_job@yahoo.com>
2022-05-10 17:35:47 +0530

    modified for seek asia: frameworks will be dragged and dropped rather than pod

---
+++ RewardzCommonComponents-Swift.h
@@ -0,0 +1,13 @@
+//
+//  RewardzCommonComponents-Swift.h
+//  RewardzCommonComponents
+//
+//  Created by Ankit on 10/05/22.
+//
+
+#ifndef RewardzCommonComponents_Swift_h
+#define RewardzCommonComponents_Swift_h
+
+
+#endif /* RewardzCommonComponents_Swift_h */
+#import "GUAAlertView.h

365ea5c7b2bd3d6ec31fb1fb86c50f50a8726e80
suyeshkandpal28 <suyesh.kandpal2@gmail.com>
2023-05-04 17:14:34 +0530

    added formatter

--- RewardzCommonComponents/Extensions/String+Extensions.swift
+++ RewardzCommonComponents/Extensions/String+Extensions.swift
@@ -76,5 +76,13 @@ public extension String {
     }
 }
 
+extension Date {
+    func string(format: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.string(from: self)
+    }
+}
+
 
 
